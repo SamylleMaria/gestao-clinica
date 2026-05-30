ff3578cdea15fbe7152001ffbe0d67bd11f672a9
samyllemaria <samyllemaria@outlook.com>
2026-05-30 12:47:04 -0300

    implementa rota GET /pacientes/:id

--- src/server.js
+++ src/server.js
@@ -5,17 +5,6 @@ import { incrementarId, validarDados } from "./utils/validacoes.js";
 const app = express();
 app.use(express.json());
 
-app.get("/pacientes", (req, res) => {
-  const gravidadeBuscada = req.query.gravidade;
-
-  if (gravidadeBuscada) {
-    const pacientesFiltrados = pacientes.filter((p) => p.gravidade === gravidadeBuscada.trim().toUpperCase());
-    return res.status(200).json(pacientesFiltrados);
-  }
-
-  return res.status(200).json(pacientes);
-});
-
 app.post("/pacientes", (req, res) => {
   const paciente = req.body;
   if (!validarDados(paciente)) {
@@ -37,6 +26,35 @@ app.post("/pacientes", (req, res) => {
   });
 });
 
+app.get("/pacientes", (req, res) => {
+  const gravidadeBuscada = req.query.gravidade;
+
+  if (gravidadeBuscada) {
+    const pacientesFiltrados = pacientes.filter(
+      (p) => p.gravidade === gravidadeBuscada.trim().toUpperCase(),
+    );
+    return res.status(200).json(pacientesFiltrados);
+  }
+
+  return res.status(200).json(pacientes);
+});
+
+app.get("/pacientes/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const pacienteBuscado = pacientes.find(
+    (pacienteBuscado) => pacienteBuscado.id === id,
+  );
+
+
+  if (!pacienteBuscado) {
+    return res.status(404).json({ erro: "Paciente não encontrado" });
+  }
+
+  return res.status(200).json(pacienteBuscado);
+});
+
+// ----
 app.listen(3030, () => {
   console.log("servidor rodando em http://localhost:3030");
 });
