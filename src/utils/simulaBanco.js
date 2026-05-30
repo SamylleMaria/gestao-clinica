9b30569b0653cfc859161b6ba68e8c59207602c7
samyllemaria <samyllemaria@outlook.com>
2026-05-30 14:58:37 -0300

    adiciona dados ao array para facilitar testes

--- src/utils/simulaBanco.js
+++ src/utils/simulaBanco.js
@@ -1,2 +1,40 @@
-export const pacientes = [];
+// export const pacientes = [];
+
+export const pacientes = [
+  {
+    id: 1,
+    nome: "Arthur Pendragon",
+    idade: 35,
+    sintomas: "Dor de cabeça severa e febre",
+    gravidade: "MEDIA"
+  },
+  {
+    id: 2,
+    nome: "Bruce Wayne",
+    idade: 42,
+    sintomas: "Fratura na costela e insônia",
+    gravidade: "ALTA"
+  },
+  {
+    id: 3,
+    nome: "Diana Prince",
+    idade: 28,
+    sintomas: "Monitoramento de rotina",
+    gravidade: "BAIXA"
+  },
+  {
+    id: 4,
+    nome: "Clark Kent",
+    idade: 33,
+    sintomas: "Exposição a minerais verdes e fraqueza",
+    gravidade: "ALTA"
+  },
+  {
+    id: 5,
+    nome: "Barry Allen",
+    idade: 25,
+    sintomas: "Metabolismo acelerado e fadiga",
+    gravidade: "BAIXA"
+  }
+];
 
