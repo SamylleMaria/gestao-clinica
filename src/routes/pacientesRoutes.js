912032f534a57fd8710fb237c54f4ae9eafef7ae
samyllemaria <samyllemaria@outlook.com>
2026-05-30 16:50:51 -0300

    refator: cria camada pacientesRoutes para separar a responsabilidade do server.js

---
+++ src/routes/pacientesRoutes.js
@@ -0,0 +1,95 @@
+import express from "express";
+import { pacientes } from "../utils/simulaBanco.js";
+import {
+  buscarId,
+  incrementarId,
+  validarDados,
+  validarStatus,
+} from "../utils/utils.js";
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  const paciente = req.body;
+  if (!validarDados(paciente)) {
+    return res.status(400).json({ erro: "Dados invávlidos" });
+  }
+
+  const pacienteCadastrado = {
+    id: incrementarId(pacientes) + 1,
+    nome: paciente.nome.trim(),
+    idade: paciente.idade,
+    sintomas: paciente.sintomas,
+    gravidade: paciente.gravidade.trim().toUpperCase(),
+  };
+  pacientes.push(pacienteCadastrado);
+  console.log(pacientes);
+  return res.status(201).json({
+    mensagem: "Paciente criado com sucesso",
+    dados: pacienteCadastrado,
+  });
+});
+
+router.get("/", (req, res) => {
+  const gravidadeBuscada = req.query.gravidade;
+
+  if (gravidadeBuscada) {
+    const pacientesFiltrados = pacientes.filter(
+      (p) => p.gravidade === gravidadeBuscada.trim().toUpperCase(),
+    );
+    return res.status(200).json(pacientesFiltrados);
+  }
+
+  return res.status(200).json(pacientes);
+});
+
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const pacienteBuscado = buscarId(id);
+
+  if (!pacienteBuscado) {
+    return res.status(404).json({ erro: "Paciente não encontrado" });
+  }
+
+  return res.status(200).json(pacienteBuscado);
+});
+
+router.put("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const status = req.body;
+
+  const pacienteBuscado = buscarId(id);
+
+  if (!pacienteBuscado) {
+    return res.status(404).json({ erro: "Paciente não encontrado" });
+  }
+
+  if (!validarStatus(status)) {
+    return res.status(400).json({ erro: "Status Inválido" });
+  }
+
+  pacienteBuscado.statusAtendimento = status.statusAtendimento
+    .trim()
+    .toUpperCase();
+  return res
+    .status(200)
+    .json({ mensagem: "Paciente modificado", dados: pacienteBuscado });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const paciente = buscarId(id);
+
+  if (!paciente) {
+    return res.status(404).json({ erro: "Paciente não encontrado" });
+  }
+
+  const indice = pacientes.findIndex((paciente) => paciente.id === id);
+
+  pacientes.splice(indice, 1);
+
+  return res.status(204).send();
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,100 +1,14 @@
-import express from "express";
-import { pacientes } from "./utils/simulaBanco.js";
-import {
-  buscarId,
-  incrementarId,
-  validarDados,
-  validarStatus,
-} from "./utils/validacoes.js";
+import express from 'express';
+import pacientesRoutes from './routes/pacientesRoutes.js'
 
 const app = express();
 app.use(express.json());
-
-app.post("/pacientes", (req, res) => {
-  const paciente = req.body;
-  if (!validarDados(paciente)) {
-    return res.status(400).json({ erro: "Dados invávlidos" });
-  }
-
-  const pacienteCadastrado = {
-    id: incrementarId(pacientes) + 1,
-    nome: paciente.nome.trim(),
-    idade: paciente.idade,
-    sintomas: paciente.sintomas,
-    gravidade: paciente.gravidade.trim().toUpperCase(),
-  };
-  pacientes.push(pacienteCadastrado);
-  console.log(pacientes);
-  return res.status(201).json({
-    mensagem: "Paciente criado com sucesso",
-    dados: pacienteCadastrado,
-  });
-});
-
-app.get("/pacientes", (req, res) => {
-  const gravidadeBuscada = req.query.gravidade;
-
-  if (gravidadeBuscada) {
-    const pacientesFiltrados = pacientes.filter(
-      (p) => p.gravidade === gravidadeBuscada.trim().toUpperCase(),
-    );
-    return res.status(200).json(pacientesFiltrados);
-  }
-
-  return res.status(200).json(pacientes);
-});
-
-app.get("/pacientes/:id", (req, res) => {
-  const id = Number(req.params.id);
-
-  const pacienteBuscado = buscarId(id);
-
-  if (!pacienteBuscado) {
-    return res.status(404).json({ erro: "Paciente não encontrado" });
-  }
-
-  return res.status(200).json(pacienteBuscado);
-});
-
-app.put("/pacientes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const status = req.body;
-
-  const pacienteBuscado = buscarId(id);
-
-  if (!pacienteBuscado) {
-    return res.status(404).json({ erro: "Paciente não encontrado" });
-  }
-
-  if (!validarStatus(status)) {
-    return res.status(400).json({ erro: "Status Inválido" });
-  }
-
-  pacienteBuscado.statusAtendimento = status.statusAtendimento
-    .trim()
-    .toUpperCase();
-  return res
-    .status(200)
-    .json({ mensagem: "Paciente modificado", dados: pacienteBuscado });
-});
-
-app.delete("/pacientes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const paciente = buscarId(id);
+app.use('/pacientes', pacientesRoutes);
 
 
-  if (!paciente) {
-    return res.status(404).json({ erro: 'Paciente não encontrado'})
-  }
-  
-  const indice = pacientes.findIndex(paciente => paciente.id === id);
-
-  pacientes.splice(indice, 1)
-
-  return res.status(204).send()
-
-
-});
+app.get('/', (req, res) => {
+  return res.json({ mensagem: 'ola, mundo'})}
+)
 
 // ----
 app.listen(3030, () => {
