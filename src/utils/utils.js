813582ea37229d6441446459153cce935f2e0a6e
samyllemaria <samyllemaria@outlook.com>
2026-06-07 16:14:49 -0300

    remove funcao utilitaria incrementarId

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,3 @@
-import { pacientes } from "./simulaBanco.js";
-
-export function incrementarId(lista) {
-  if (lista.length === 0) return 0;
-
-  return Math.max(...lista.map((item) => item.id));
-}
-
-
 export function validarGravidade(gravidade) {
   const opcoesValidas = ["BAIXA", "MEDIA", "ALTA"];
   return opcoesValidas.includes(gravidade.trim().toUpperCase());
